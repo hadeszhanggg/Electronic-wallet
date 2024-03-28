52a7d8a566587edb066c5e5548bf86d3504ca4ec
hadeszhangg <truongdangminhtan333@gmail.com>
2024-03-28 15:36:58 +0700

    Update new controllers: getAllVouchers, getUnusedVouchers, getUnpaidBills

--- src/controllers/user.Controllers.js
+++ src/controllers/user.Controllers.js
@@ -6,11 +6,10 @@ exports.handleWelcomeMessage=async(req,res)=> {
     console.log('Received welcome message:', message);
 };
 
-// Function to get all bills of a user based on accessToken
-exports.getAllBillsByAccessToken = async (req, res) => {
+exports.getAllBills = async (req, res) => {
     try {
       const userId = req.userId;
-      // Find walletId corresponding to userId
+      // tìm ví dựa trên userID 
       const userWallet = await db.wallet.findOne({ where: { userId: userId } });
       
       if (!userWallet) {
@@ -19,14 +18,81 @@ exports.getAllBillsByAccessToken = async (req, res) => {
   
       const walletId = userWallet.id;
   
-      // Find all bills associated with the user's walletId
-      const bills = await db.bill.findAll({ where: { walletId: walletId } });
+      // Lấy tất cả bills thuộc ví (walletId)
+      const bills = await db.bill.findAll({ 
+        where: { walletId: walletId },
+        attributes: ['id', 'description', 'total', 'paid', 'expiryDays', 'paid_date']
+    });
   
       return res.status(200).json(bills);
     } catch (error) {
       console.error("Error:", error);
       return res.status(500).send({ message: "Internal Server Error" });
     }
-  };
+};
+exports.getAllVouchers = async (req, res) => {
+    try {
+      const userId = req.userId;
+      // tìm ví dựa trên userID 
+      const userWallet = await db.wallet.findOne({ where: { userId: userId } });
+      
+      if (!userWallet) {
+        return res.status(404).send({ message: "User wallet not found" });
+      }
+  
+      const walletId = userWallet.id;
+  
+      // Lấy tất cả vouchers thuộc ví (walletId)
+     // const vouchers = await db.voucher.findAll({ where: { walletId: walletId } });
+      const vouchers = await db.voucher.findAll({ 
+        where: { walletId: walletId },
+        attributes: ['id', 'voucher_name', 'description', 'discount', 'exp', 'used','used_date']
+    });
+      return res.status(200).json(vouchers);
+    } catch (error) {
+      console.error("Error:", error);
+      return res.status(500).send({ message: "Internal Server Error" });
+    }
+};
+exports.getUnpaidBills = async (req, res) => {
+    try {
+        const userId = req.userId;
+        // Tìm ví dựa trên userID
+        const userWallet = await db.wallet.findOne({ where: { userId: userId } });
+        if (!userWallet) {
+            return res.status(404).send({ message: "User wallet not found" });
+        }
+        const walletId = userWallet.id;
+        // Lấy tất cả các hóa đơn chưa thanh toán thuộc ví (walletId)
+        const unpaidBills = await db.bill.findAll({
+            where: { walletId: walletId, paid: false },
+            attributes: ['id', 'description', 'total', 'expiryDays', 'paid_date']
+        });
+        return res.status(200).json(unpaidBills);
+    } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).send({ message: "Internal Server Error" });
+    }
+};
 
+exports.getUnusedVouchers = async (req, res) => {
+    try {
+        const userId = req.userId;
+        // Tìm ví dựa trên userID
+        const userWallet = await db.wallet.findOne({ where: { userId: userId } });
 
+        if (!userWallet) {
+            return res.status(404).send({ message: "User wallet not found" });
+        }
+        const walletId = userWallet.id;
+        // Lấy tất cả các voucher chưa sử dụng thuộc ví (walletId)
+        const unusedVouchers = await db.voucher.findAll({
+            where: { walletId: walletId, used: false },
+            attributes: ['id', 'voucher_name', 'description', 'discount', 'exp']
+        });
+        return res.status(200).json(unusedVouchers);
+    } catch (error) {
+        console.error("Error:", error);
+        return res.status(500).send({ message: "Internal Server Error" });
+    }
+};
--- src/routes/user.Routes.js
+++ src/routes/user.Routes.js
@@ -10,12 +10,43 @@ module.exports = function (app) {
         );
         next();
     });
+     //Route lấy tất cả bills của user dựa trên accessToken của user
     app.get('/users/getAllBills', authJwt.authenticateToken,  authJwt.logUserInfo, async (req, res) => {
         try {
-            await controllers.getAllBillsByAccessToken(req,res);
+            await controllers.getAllBills(req,res);
             logging.info(`get all bills successfully from user ID: [${req.userId}], email: [${req.userEmail}] and client IP: [${req.clientIp}]`) 
         } catch (error) {
             logging.error(`get all bills failed with detail: [${error.message}] from user ID: [${req.userId}], email: [${req.userEmail}] and client IP: [${req.clientIp}], from routes: /users/getAllBills`);
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    });
+    //Route lấy tất cả vouchers của user dựa trên accessToken của user 
+    app.get('/users/getAllVouchers', authJwt.authenticateToken,  authJwt.logUserInfo, async (req, res) => {
+        try {
+            await controllers.getAllVouchers(req,res);
+            logging.info(`get all vouchers successfully from user ID: [${req.userId}], email: [${req.userEmail}] and client IP: [${req.clientIp}]`) 
+        } catch (error) {
+            logging.error(`get all vouchers failed with detail: [${error.message}] from user ID: [${req.userId}], email: [${req.userEmail}] and client IP: [${req.clientIp}], from routes: /users/getAllBills`);
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    });
+     //Route lấy danh sách các vouchers chưa sử dụng của user dựa trên accessToken của user
+     app.get('/users/getUnusedVouchers', authJwt.authenticateToken,  authJwt.logUserInfo, async (req, res) => {
+        try {
+            await controllers.getUnusedVouchers(req,res);
+            logging.info(`get unused voucher list successfully from user ID: [${req.userId}], email: [${req.userEmail}] and client IP: [${req.clientIp}]`) 
+        } catch (error) {
+            logging.error(`get unused voucher list failed with detail: [${error.message}] from user ID: [${req.userId}], email: [${req.userEmail}] and client IP: [${req.clientIp}], from routes: /users/getAllBills`);
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    });
+      //Route lấy danh sách các bills chưa thanh toán của user dựa trên accessToken của user
+     app.get('/users/getUnpaidBills', authJwt.authenticateToken,  authJwt.logUserInfo, async (req, res) => {
+        try {
+            await controllers.getUnpaidBills(req,res);
+            logging.info(`get unpaid bill list successfully from user ID: [${req.userId}], email: [${req.userEmail}] and client IP: [${req.clientIp}]`) 
+        } catch (error) {
+            logging.error(`get unpaid bill list failed with detail: [${error.message}] from user ID: [${req.userId}], email: [${req.userEmail}] and client IP: [${req.clientIp}], from routes: /users/getAllBills`);
             res.status(500).json({ message: "Internal Server Error" });
         }
     });
